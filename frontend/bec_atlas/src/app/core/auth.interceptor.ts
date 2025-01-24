c03705c2cc2e9764d6074a20a743127d0cc84fc0
wakonig_k <klaus.wakonig@psi.ch>
2025-01-24 14:23:27 +0100

    fix(interceptor): added missing files

---
+++ frontend/bec_atlas/src/app/core/auth.interceptor.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+function logout() {
+  localStorage.removeItem('id_token');
+  localStorage.removeItem('id_session');
+  location.href = '/login';
+}
+
+function handle_request(handler: HttpHandler, req: HttpRequest<any>) {
+  return handler.handle(req).pipe(
+    tap(
+      (event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          // console.log(cloned);
+          // console.log("Service Response thr Interceptor");
+        }
+      },
+      (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          console.log('err.status', err);
+          if (err.status === 401) {
+            logout();
+          }
+        }
+      }
+    )
+  );
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const idToken = localStorage.getItem('id_token');
+
+    if (idToken) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', 'Bearer ' + idToken),
+      });
+
+      return handle_request(next, cloned);
+    } else {
+      return handle_request(next, req);
+    }
+  }
+}
