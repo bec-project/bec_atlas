73b2928aadc444f284f9de1ffb294126290e79fc
wyzula-jan <jan.wyzula@psi.ch>
2025-12-17 14:54:11 +0100

    AG-MC: wip very basic button

--- frontend/bec_atlas_mc/src/app/app.ts
+++ frontend/bec_atlas_mc/src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, effect, signal } from '@angular/core';
+import { MyButton } from './ui/my-button/my-button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MyButton, MatCardModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('bec_atlas_mc');
+  readonly count = signal(0);
+
+  isDisabled = computed(() => this.count() >= 3);
+  buttonLabel = computed(() =>
+    this.isDisabled() ? 'Limit reached HEHE (3)' : `Click me (${this.count()})`
+  );
+
+  constructor() {
+    effect(() => {
+      // Runs whenever count changes
+      console.log('count =', this.count());
+    });
+  }
+
+  increment(): void {
+    this.count.update((v) => v + 1);
+  }
+
+  reset(): void {
+    this.count.set(0);
+  }
 }
--- frontend/bec_atlas_mc/src/app/ui/my-button/my-button.ts
+++ frontend/bec_atlas_mc/src/app/ui/my-button/my-button.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {MatButton, MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-my-button',
-  imports: [],
+  imports: [MatButtonModule, MatButton],
   templateUrl: './my-button.html',
   styleUrl: './my-button.css',
 })
 export class MyButton {
+  @Input() label = 'Click';
+  @Input() disabled = false;
 
+  @Output() clicked = new EventEmitter<void>();
+
+  onClick(): void {
+    if (!this.disabled) {
+      this.clicked.emit();
+    }
+  }
 }
